22d32533b39866e7d92c970ec0116c56b03a72ff
roman <romyli4ka@gmail.com>
2021-12-01 13:34:36 -0500

    frontend wordserch base

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,19 @@
+import Artists from "../components/Artists";
+import * as CONSTANTS from "../components/constants";
+
+export default {
+   SetupFooter,
+}
+
+export function SetupFooter(){
+   return`
+   <div class="footer">
+   <ul>
+            <li>Dawn Natale -- </li>
+            <li>Eli Heeren --</li>
+            <li>Jaxxon Crowe --</li>
+            <li>Jay Huang --</li>
+            <li>Roman Shmigelskyi</li>
+          </ul></div>
+   `;
+}
