99bc9ae5559ca236e80b16d728ed81f5c8dc2d1d
roman <romyli4ka@gmail.com>
2021-12-05 20:32:04 -0500

    corected db wordserch game

--- backend/GameContext.cs
+++ backend/GameContext.cs
@@ -421,10 +421,10 @@ namespace EducationalGameMaker
                 {
                     Id = 2,
                     UserId = 1,
-                    Matrix = "j,e,f,b,l,p,e,p,r,e,c,a,m,d,c,i,m,g,t,c,o,i,v,o,k,p,r,j,s,m,p,b,w,a,s,q,r,o,u,a,r,i,x,i,l,e,l,h,r,s,w,o,l,c,q,l,i,r,p,c,s,c,r,e,e,a,u,m,g,r,a,l,x,h,p,b,u,r,y,i,j,a,l,a,y,c,a,l,m,p,c,l,o,j,u,r,e,r,m,t",
+                    Matrix = "j, e, f, b, l, p, e, p, r, e, c, a, m, d, c, i, m, g, t, c, o, i, v, o, k, p, r, j, s, m, p, b, w, a, s, q, r, o, u, a, r, i, x, i, l, e, l, h, r, s, w, o, l, c, q, l, i, r, p, c, s, c, r, e, e, a, u, m, g, r, a, l, x, h, p, b, u, r, y, i, j, a, l, a, y, c, a, l, m, p, c, l, o, j, u, r, e, r, m, t",
                     Height = 10,
                     Width = 10,
-                    Keywords = "java"
+                    Keywords = "java, ruby"
                 }
 
 
--- backend/Migrations/20211205171812_corected.cs
+++ backend/Migrations/20211205171812_corected.cs
@@ -21,7 +21,7 @@ namespace EducationalGameMaker.Migrations
                 keyColumn: "Id",
                 keyValue: 1,
                 column: "Matrix",
-                value: "J, Q, V, N, R, G, N, T, U, I, V, L, A, O, S, A, X, P, S, C,W, P, R, I, D, M, E, I, I, T, K, P, I, T, I, E, T, R, H, K, Z, U, A, C, K, S, G, C, X, T, X, T, B, N, F, Z, Y, S, G, J, O, D, L, U, Y, U, H, A, H, N, A, F, E, F, C, A, N, V, A, S, E, D, O, C, R, P, L, A, Y, A, F, X, E, E, V, C, P, J, H, B");
+                value: "J, Q, V, N, R, G, N, T, U, I, V, L, A, O, S, A, X, P, S, C, W, P, R, I, D, M, E, I, I, T, K, P, I, T, I, E, T, R, H, K, Z, U, A, C, K, S, G, C, X, T, X, T, B, N, F, Z, Y, S, G, J, O, D, L, U, Y, U, H, A, H, N, A, F, E, F, C, A, N, V, A, S, E, D, O, C, R, P, L, A, Y, A, F, X, E, E, V, C, P, J, H, B");
         }
     }
 }
