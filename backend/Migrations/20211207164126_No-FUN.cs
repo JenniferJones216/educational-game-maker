1c2452f054f9d7ff465c0d7795b1a1853ab8e08e
roman <romyli4ka@gmail.com>
2021-12-07 13:40:53 -0500

    roman

---
+++ backend/Migrations/20211207164126_No-FUN.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EducationalGameMaker.Migrations
+{
+    public partial class NoFUN : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Wordsearches",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Keywords",
+                value: "CANVAS, CODE, FUNCTION, GAMES, JAVASCRIPT, KIDS, PLAY, SHARE, VARIABLE");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Wordsearches",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Keywords",
+                value: "CANVAS, CODE, FUN, FUNCTION, GAMES, JAVASCRIPT, KIDS, PLAY, SHARE, VARIABLE");
+        }
+    }
+}
