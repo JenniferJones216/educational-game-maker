34be64aadc0e7a24970227af002e7638622c23e1
1Jhovany <cuevasjhovany@gmail.com>
2021-12-09 10:14:45 -0600

    olchanges

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,13 +9,12 @@ function displayHome(){
     <h1 id="title">Educational Game Maker</h1>
     <div class="container">
     <h4 id="subHead">Directions</h4>
-          <ol id="text">
-            <li>Remember have fun !!</li>
-            <li>Choose whatever game through navbar</li>
-            <li>Then either create or play it</li>
-            <li>It's all up to you</li>
-          </ol>
-        </div>
+          <div id="text">
+            <p>Remember have fun !!</p>
+            <p>Choose whatever game through navbar</p>
+            <p>Then either create a game or play it</p>
+            <p>It's all up to you</p>
+          </div>
     </div>
     `;
 }
