bcef844be3db69940887f3217c6cc707e998b501
JenniferJones216 <jenniferjones216@gmail.com>
2021-12-09 23:57:00 -0500

    dnd css

--- frontend/src/components/DragAndDrop.js
+++ frontend/src/components/DragAndDrop.js
@@ -1,58 +1,10 @@
 import * as CONSTANTS from "../components/constants";
 
 export default {
-    displayDragAndDrop,
     SetUpDragFunctions
 }
 
-function displayDragAndDrop() {
-    return `
-    <h1 id="dragAndDropTitle">An Issue in Health Care: Illiteracy
-    </h1>
-    <h4 id="dragAndDropSubheading">Directions: Click and drag the phrases to the correct spaces in the text.</h4>
-    <div class="container">
-    </br>
-        <div class="drop-targets" id="drop-targets">
-            <span class='promptText'>
-            According to some estimates, well over half of the people in the world are functionally illiterate. This means that they are unable to perform everyday tasks because they can't read, understand, and respond appropriately to information. One of the problems this creates in health care is that millions of people are not</span>
-            <span class='box'>able to read</span>
-            <span class='promptText'>directions on medicine bottles or packages. Imagine</span>
-            <span class='box'>being</span>
-            <span class='promptText'>a parent with a sick child and being unable</span>
-            <span class='box'>to read</span>
-            <span class='promptText'> the directions on a medicine bottle. We all know that it is important for medical directions</span>
-            <span class='box'>to be understood</span>
-            <span class='promptText'>clearly. One solution is pictures. Many medical professionals are working today</span>
-            <span class='box'>to solve</span>
-            <span class='promptText'>this problem by</span>
-            <span class='box'>using</span>
-            <span class='promptText'>pictures to convey health-care information.</span>
-            </div>
-        <div class="wordbox" id="word-bank">
-            <h3>Word Bank</h3>
-            <div class='item' id='1' draggable='true'>
-                to be understood
-            </div>
-            <div class='item' id='2' draggable='true'>
-                able to read
-            </div>
-            <div class='item' id='3' draggable='true'>
-               to solve
-            </div>
-            <div class='item' id='4' draggable='true'>
-                using
-            </div>
-            <div class='item' id='5' draggable='true'>
-                to read
-            </div>
-            <div class='item' id='6' draggable='true'>
-                being
-            </div>
-        </div>
-    </div>
-    `;
 
-}
 
 function SetUpDragFunctions() {
 
--- frontend/src/components/LoadDrag.js
+++ frontend/src/components/LoadDrag.js
@@ -4,27 +4,7 @@ export default {
     
     }
 
-// function displayDragon(DragandDrop) {
-//     return `
-//     <h1 id="dragAndDropTitle">${DragandDrop.title}
-//     </h1>
-//     <h4 id="dragAndDropSubheading">${DragandDrop.subheading}</h4>
-//     <div class="container">
-//     </br>
-//         <div class="drop-targets" id="drop-targets">
-        
-//             ${DragandDrop.promptText}
-//             </div>
-//         <div class="wordbox" id="word-bank">
-//             <h3>Word Bank</h3>
-//             ${DragandDrop.wordBank}
-//         </div>
-//         </br>
-        
-//     </div>
-//     `;
 
-// }
 
 function displayDragAndDrop(dragandDrop) {
     return `
@@ -32,7 +12,7 @@ function displayDragAndDrop(dragandDrop) {
 <h1 id="dragAndDropTitle">${dragandDrop.title}
     </h1>
     <h4 id="dragAndDropSubheading">${dragandDrop.subheading}</h4>
-    <div class="container">
+    <div id="dndcontainer" class="container">
     </br>
       ${dragandDrop.promptText}
 
